d0b830f2c087f6b8b1611e99b09847ca20f5e8cb
Roshan kumar <roshankumarskr@gmail.com>
2025-05-02 21:47:45 +0530

    data files

---
+++ utils/data/projects-data.js
@@ -0,0 +1,52 @@
+export const projectsData = [
+    {
+        id: 1,
+        name: 'NFT Marketplace on IC Blockchain App',
+        description: "Built an NFT marketplace similar to OpenSea where users can buy and sell NFTs. The platform uses the Internet Computer blockchain, providing a smooth and efficient user experience.",
+        tools: ['Motoko', 'Python', 'TypeScript', 'API', 'React', 'Minting', 'Internet Auth', 'EJS'],
+        role: 'Full Stack Developer',
+        code: '',
+        demo: '',
+    },
+    {
+        id: 2,
+        name: 'SQLancer DBMS Orchestration & Automation',
+        description: "Contributed to the SQLancer project by building a CLI-based tool to automate the setup, execution, and result collection of tests across various DBMSs like PostgreSQL, MySQL, SQLite, MariaDB, DuckDB, and CockroachDB. Integrated Docker for containerized DBMS environments and added support for dynamic LLM-powered test configuration. The tool improves reproducibility and reduces manual intervention, helping researchers and developers test SQLancer more efficiently.",
+        tools: ['Python', 'Docker', 'SQLancer', 'Bash', 'CockroachDB', 'PostgreSQL', 'MySQL', 'MariaDB', 'DuckDB', 'SQLite', 'TogetherAI'],
+        role: 'Open Source Contributor & Automation Developer',
+        code: 'https://github.com/roshan-ai/sqlancer-orchestration-tool',
+        demo: '',
+    },
+    {
+        id: 3,
+        name: 'Rizhu Token on Blockchain',
+        description: "Developed a blockchain token on the Internet Computer platform that allows users to securely transfer and receive funds with double authentication. The UI is inspired by 90s websites to enhance trust and transparency.",
+        tools: ['Dfinity', 'Motoko', 'TypeScript', 'React', 'Auth', 'API', 'Canister'],
+        code: '',
+        role: 'Full Stack Developer',
+        demo: '',
+    },
+    {
+        id: 4,
+        name: 'To-Do List Web Application',
+        description: "Created a highly efficient and customizable to-do list web application. Features include cache-based login (no repeated logins), customizable lists, background themes, alarm notifications, and a minimalist design.",
+        tools: ['React', 'MongoDB', 'EJS', 'Tailwind', 'Cache Login'],
+        code: '',
+        demo: '',
+        role: 'Full Stack Developer',
+    }
+];
+
+
+// Do not remove any property.
+// Leave it blank instead as shown below
+
+// {
+//     id: 1,
+//     name: '',
+//     description: "",
+//     tools: [],
+//     role: '',
+//     code: '',
+//     demo: '',
+// },
