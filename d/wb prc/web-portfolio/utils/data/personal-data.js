8584e1c8253518b24cfc5537d6563decc1c5ede2
Roshan kumar <roshankumarskr@gmail.com>
2025-12-31 06:39:02 +0530

    try

---
+++ d/wb prc/web-portfolio/utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "ROSHAN KUMAR",
+  profile: '/profile.png',
+  designation: "Software Developer",
+  description: "I'm Roshan Kumar, a passionate full-stack developer with a strong foundation in JavaScript and modern web technologies like React, Next.js, and Node.js. I enjoy solving real-world problems, building scalable web applications, and exploring new tech. With experience in DevOps tools like Docker, GitHub Actions, and cloud platforms, I focus on delivering efficient, end-to-end solutions. I'm a fast learner, open to new challenges, and always excited to contribute to meaningful projects.",
+  email: 'roshanhere03@gmail.com',
+  phone: '+91 7368827906',
+  address: 'yehalaka NES, Banglore, India  ',
+  github: 'https://github.com/roshankumar1204',
+  facebook: 'https://www.facebook.com/',
+  linkedIn: 'https://www.linkedin.com/in/roshan-kumar1204',
+  twitter: 'https://twitter.com/',
+  stackOverflow: 'https://stackoverflow.com/',
+  leetcode: "https://leetcode.com/u/roshantheAWkward/",
+  devUsername: "roshan_1204",
+  resume: "https://drive.google.com/file/d/1BDMVxBpXY5q9YH8j75HcQjrM92SG2oGm/view?usp=drive_link"
+}
